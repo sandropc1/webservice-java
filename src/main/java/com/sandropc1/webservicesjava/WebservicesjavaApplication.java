f0c05dfabf55a4d5f5547ba550268a24dd821b36
sandro <sandrosqrt@gmail.com>
2026-02-18 14:38:46 -0300

    Project Setup

---
+++ src/main/java/com/sandropc1/webservicesjava/WebservicesjavaApplication.java
@@ -0,0 +1,13 @@
+package com.sandropc1.webservicesjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicesjavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebservicesjavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sandropc1/webservicesjava/WebservicesjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandropc1.webservicesjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesjavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
