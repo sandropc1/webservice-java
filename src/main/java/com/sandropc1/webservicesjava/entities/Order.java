3f4fc84a4ff806150b154ca2a46f18308bc28756
sandro <sandrosqrt@gmail.com>
2026-02-19 16:34:28 -0300

    Total e Subtotal methods

--- src/main/java/com/sandropc1/webservicesjava/entities/Order.java
+++ src/main/java/com/sandropc1/webservicesjava/entities/Order.java
@@ -36,7 +36,6 @@ public class Order implements Serializable {
     private Payment payment;
 
     public Order(){
-
     }
 
     public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
@@ -90,6 +89,14 @@ public class Order implements Serializable {
 
     public void setPayment(Payment payment) {
         this.payment = payment;
+    }
+
+    public Double getTotal(){
+        double sum = 0.0;
+        for (OrderItem x : items) {
+            sum = sum + x.getSubTotal();
+        }
+        return sum;
     }
 
     @Override
