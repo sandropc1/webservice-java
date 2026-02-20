da318d21cc11ca50ca406aa2567b4a0cc092ebee
sandro <sandrosqrt@gmail.com>
2026-02-19 23:00:13 -0300

    Handling delete User exception

--- src/main/java/com/sandropc1/webservicesjava/services/UserService.java
+++ src/main/java/com/sandropc1/webservicesjava/services/UserService.java
@@ -2,8 +2,11 @@ package com.sandropc1.webservicesjava.services;
 
 import com.sandropc1.webservicesjava.entities.User;
 import com.sandropc1.webservicesjava.repositories.UserRepository;
+import com.sandropc1.webservicesjava.services.exceptions.DatabaseException;
 import com.sandropc1.webservicesjava.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        }catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
